cde32c9078c6b4e1b3b72458a87309a9046c22af
Sal <sal.abuewilly@centrica.com>
2018-10-20 22:22:54 +0100

    Examples of observers and computed properties

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,17 @@
 import Controller from '@ember/controller';
-
+import {computed, observer} from '@ember/object';
 export default Controller.extend({
-  isDisabled: true
+  isDisabled: computed('emailAddress', function() {
+    return this.get('emailAddress') === '';
+  }),
+  emailAddress: '',
+  actualEmailAddress: computed('emailAddress', function() {
+    // Called only when you go and use that property
+    console.log('ActualEmailAddress function is called: ', this.get('emailAddress'));
+    
+  }),
+  emailAddressChanged: observer('emailAddress', function() {
+    //will always be called when the value of the emailAddress changes
+    console.log('observer is called', this.get('emailAddress'));
+  })
 });
