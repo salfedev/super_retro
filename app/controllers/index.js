65ce42c29017cbac9ec7fb6cf88033fbf98c3e78
Sal <sal.abuewilly@centrica.com>
2018-10-20 22:28:13 +0100

    Refactor isDisabled using computed.empty()

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,17 +1,18 @@
 import Controller from '@ember/controller';
 import {computed, observer} from '@ember/object';
+import { empty } from '@ember/object/computed';
 export default Controller.extend({
-  isDisabled: computed('emailAddress', function() {
-    return this.get('emailAddress') === '';
-  }),
+  isDisabled: empty('emailAddress'),
   emailAddress: '',
   actualEmailAddress: computed('emailAddress', function() {
     // Called only when you go and use that property
+    // eslint-disable-next-line
     console.log('ActualEmailAddress function is called: ', this.get('emailAddress'));
     
   }),
   emailAddressChanged: observer('emailAddress', function() {
     //will always be called when the value of the emailAddress changes
+    // eslint-disable-next-line
     console.log('observer is called', this.get('emailAddress'));
   })
 });
