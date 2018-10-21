02904a1763fed9fc600a8e9cdf958e558df12d1a
Sal <sal.abuewilly@centrica.com>
2018-10-21 02:35:51 +0100

    Add form validations with computed.observer

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,13 +1,23 @@
 import Controller from '@ember/controller';
+import { observer } from "@ember/object";
 import {not, match, gte, and} from '@ember/object/computed';
 
 export default Controller.extend({
+  emailTouched: observer('emailAddress', function() {
+    this.set('isEmailTouched', true)
+  }),
+  messageTouched: observer('messageBody', function() {
+    this.set('isMessageTouched', true)
+  }),
+
   emailAddress: '',
   messageBody: '',
+
   isDisabled: not('isFormValid'),
   isEmailValid: match('emailAddress', /^.+@.+\..+$/),
   isMessageValid: gte('messageBody.length', 20),
   isFormValid: and('isEmailValid', 'isMessageValid'),
+
   actions: {
     sendMessage() {
       // alert(`Creating issue now using username: ${this.get('emailAddress')}`);
