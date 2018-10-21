47981f673455967a51024f58c6ff8ba6cb69827f
Sal <sal.abuewilly@centrica.com>
2018-10-21 01:56:33 +0100

    Add successMessage and dissmiss action

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,20 +3,25 @@ import {computed, observer} from '@ember/object';
 import { not, match } from '@ember/object/computed';
 export default Controller.extend({
   headerMessage: "Welcome to Super Retro.",
+
   isDisabled: not('isValid'),
   isValid: match('emailAddress', /^.+@.+\..+$/),
+
   emailAddress: '',
+
   actualEmailAddress: computed('emailAddress', function() {
     // Called only when you go and use that property
     // eslint-disable-next-line
-    console.log('ActualEmailAddress function is called: ', this.get('emailAddress'));
+    // console.log('ActualEmailAddress function is called: ', this.get('emailAddress'));
     
   }),
+
   emailAddressChanged: observer('emailAddress', function() {
     //will always be called when the value of the emailAddress changes
     // eslint-disable-next-line
-    console.log('observer is called', this.get('emailAddress'));
+    // console.log('observer is called', this.get('emailAddress'));
   }),
+
   actions: {
     saveInvitation() {
       alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
