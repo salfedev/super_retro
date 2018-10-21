1b59e8ba4bb7fabfc5a1948ed30cc3ebd249b1c4
Sal <sal.abuewilly@centrica.com>
2018-10-21 02:08:00 +0100

    Refactor to use computed.gte : greater than or equal

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,14 +1,15 @@
 import Controller from '@ember/controller';
 import {computed} from '@ember/object';
-import {not, match} from '@ember/object/computed';
+import {not, match, gte} from '@ember/object/computed';
 
 export default Controller.extend({
   emailAddress: '',
   messageBody: '',
   isDisabled: not('isFormValid'),
   isEmailValid: match('emailAddress', /^.+@.+\..+$/),
+  isMessageValid: gte('messageBody.length', 20),
   isFormValid: computed('isEmailValid', 'messageBody', function() {
-    return this.get('isEmailValid') && this.get('messageBody').length > 20;
+    return this.get('isEmailValid') && this.get('isMessageValid');
   }),
   actions: {
     sendMessage() {
