3e4ec33f1ccaff47d84642e1a8fb61fc37613238
Sal <sal.abuewilly@centrica.com>
2018-10-21 01:55:50 +0100

    Add contact controller and form validations

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,4 +1,24 @@
 import Controller from '@ember/controller';
+import {computed} from '@ember/object';
+import {not, match} from '@ember/object/computed';
 
 export default Controller.extend({
+  emailAddress: '',
+  messageBody: '',
+  isDisabled: not('isFormValid'),
+  isEmailValid: match('emailAddress', /^.+@.+\..+$/),
+  isFormValid: computed('isEmailValid', 'messageBody', function() {
+    return this.get('isEmailValid') && this.get('messageBody').length > 20;
+  }),
+  actions: {
+    sendMessage() {
+      // alert(`Creating issue now using username: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Issue created successfully! Thanks!`)
+      this.set('emailAddress', '');
+      this.set('messageBody', '');
+    },
+    dismissAlert() {
+      this.set('responseMessage', false)
+    }
+  }
 });
