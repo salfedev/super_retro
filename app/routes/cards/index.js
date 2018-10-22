a8189e14340c6d538aaaa7026a6ab965c1405e27
Sal <sal.abuewilly@centrica.com>
2018-10-22 18:12:51 +0100

    Add deleteCard action to cards/index

--- app/routes/cards/index.js
+++ app/routes/cards/index.js
@@ -4,5 +4,13 @@ export default Route.extend({
   columnHeader: "To Do",
   model() {
     return this.store.findAll('card');
+  },
+  actions: {
+    deleteCard(card) {
+      let confirmation = confirm('Are you sure you want to delete this card?')
+      if (confirmation) {
+        card.destroyRecord();
+      }
+    }
   }
 });
